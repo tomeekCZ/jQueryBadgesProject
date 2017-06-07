74a6da4f351d77e1829f5250286bd7a18c3f72cd
TomeekCZ <tomeek.nemecek@gmail.com>
2017-06-07 13:01:15 +0200

    All done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,31 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: "https://www.codeschool.com/users/3754829.json",
+    dataType: "jsonp",
+    success: function(result){
+      var courseItems = $.map(result.courses.completed, function(item, index){
+
+          console.log(item);
+          var courseItem = $("<div></div>");
+          courseItem.addClass("course");
+          $("<h3>" + item.title + "</h3>").appendTo(courseItem);
+
+          var imageItem = $("<img></img>");
+          imageItem.attr("src", item.badge);
+          imageItem.appendTo(courseItem);
+
+          var linkItem = $("<a>See Course</a>");
+          linkItem.attr("href", item.url);
+          linkItem.attr("target", "_blank");
+          linkItem.addClass("btn btn-primary");
+          linkItem.appendTo(courseItem);
+
+          return courseItem;
+      });
+
+      $("#badges").html(courseItems);
+    }
+  })
 
 });
